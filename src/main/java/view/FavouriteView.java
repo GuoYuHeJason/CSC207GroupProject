337ea31546a416a820ec1b35faaf8e7a3bb754cd
dengxues <K1bbu7z@gmail.com>
2024-11-11 21:22:10 -0500

    FavouriteView Update

--- src/main/java/view/FavouriteView.java
+++ src/main/java/view/FavouriteView.java
@@ -14,7 +14,7 @@ import javax.swing.*;
 import use_case.favourite.adapter.FavouriteController;
 import use_case.favourite.adapter.FavouriteState;
 
-public class FavouriteView extends JPanel implements ActionListener, PropertyChangeListener {
+public class FavouriteView extends JPanel {
 
     private final FavouriteController favouriteController;
     private final FavouriteViewModel favouriteViewModel;
@@ -32,7 +32,7 @@ public class FavouriteView extends JPanel implements ActionListener, PropertyCha
     public FavouriteView(FavouriteViewModel favouriteViewModel, FavouriteController controller) {
         this.favouriteController = controller;
         this.favouriteViewModel = favouriteViewModel;
-        this.favouriteViewModel.addPropertyChangeListener(this);
+//        this.favouriteViewModel.addPropertyChangeListener(this);
 
         final JLabel title = new JLabel(FavouriteViewModel.TITLE_LABEL);
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -40,51 +40,56 @@ public class FavouriteView extends JPanel implements ActionListener, PropertyCha
         final LabelTextPanel search = new LabelTextPanel(new JLabel(FavouriteViewModel.KEYWORD_LABEL), searchBox);
 
         final JPanel buttons = new JPanel();
-        funniestButton = new JButton(FavouriteViewModel.FUNNIEST_BUTTOM_LABEL);
-        buttons.add(funniestButton);
         searchButton = new JButton(FavouriteViewModel.SEARCH_BUTTOM_LABEL);
         buttons.add(searchButton);
-        cancelButton = new JButton(FavouriteViewModel.CANCEL_BUTTOM_LABEL;
+        funniestButton = new JButton(FavouriteViewModel.FUNNIEST_BUTTOM_LABEL);
+        buttons.add(funniestButton);
+        cancelButton = new JButton(FavouriteViewModel.CANCEL_BUTTOM_LABEL);
         buttons.add(cancelButton);
 
-        funniestButton.addActionListener(
-                evt -> {
-                    if (evt.getSource().equals(funniestButton)) {
-                        favouriteController.execute(favouriteInputField.getText());
+        this.add(title);
+        this.add(search);
+        this.add(buttons);
 
-                    }
-                }
-        );
-
-        refreshButton.addActionListener(
-                evt -> {
-                    if (evt.getSource().equals(refreshButton)) {
-                        favouriteController.execute(null);
-
-                    }
-                }
-        );
-    }
-
-    /**
-     * React to a button click that results in evt.
-     * @param evt the ActionEvent to react to
-     */
-    public void actionPerformed(ActionEvent evt) {
-        System.out.println("Click " + evt.getActionCommand());
-    }
-
-    public void propertyChange(PropertyChangeEvent evt) {
-        final FavouriteState state = (FavouriteState) evt.getNewValue();
-        setFields(state);
-        usernameErrorField.setText(state.getFavouriteError());
-    }
-
-    private void setFields(FavouriteState state) {
-        usernameInputField.setText(state.getUsername());
-    }
-
-    public String getViewName() {
-        return viewName;
+//        funniestButton.addActionListener(
+//                evt -> {
+//                    if (evt.getSource().equals(funniestButton)) {
+//                        favouriteController.execute(favouriteInputField.getText());
+//
+//                    }
+//                }
+//        );
+//
+//        refreshButton.addActionListener(
+//                evt -> {
+//                    if (evt.getSource().equals(refreshButton)) {
+//                        favouriteController.execute(null);
+//
+//                    }
+//                }
+//        );
+//    }
+//
+//    /**
+//     * React to a button click that results in evt.
+//     * @param evt the ActionEvent to react to
+//     */
+//    public void actionPerformed(ActionEvent evt) {
+//        System.out.println("Click " + evt.getActionCommand());
+//    }
+//
+//    public void propertyChange(PropertyChangeEvent evt) {
+//        final FavouriteState state = (FavouriteState) evt.getNewValue();
+//        setFields(state);
+//        usernameErrorField.setText(state.getFavouriteError());
+//    }
+//
+//    private void setFields(FavouriteState state) {
+//        usernameInputField.setText(state.getUsername());
+//    }
+//
+//    public String getViewName() {
+//        return viewName;
+//    }
     }
 }
